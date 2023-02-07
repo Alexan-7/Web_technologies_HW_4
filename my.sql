4ec89ccb8528302f0d92b691fcf577f1513b8d59
Alexander Berdyugin <112006440+Alexan-7@users.noreply.github.com>
2023-02-07 17:06:35 +0300

    Update my.sql

--- my.sql
+++ my.sql
@@ -3,24 +3,24 @@
 CREATE TABLE groupmates (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   name TEXT NOT NULL,
-  age TEXT NOT NULL,
+  age INTEGER NOT NULL,
   address TEXT NOT NULL,
   floor TEXT NOT NULL
 );
 
 -- insert
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Clark', '25', 'Moscow', 'male');
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Dave', '17', 'Yakutsk', 'male');
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Ava', '26', 'Novosibirsk', 'female');
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Sasha', '46', 'St. Petersburg', 'male');
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Petya', '30', 'Neryungri', 'male');
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Masha', '18', 'Omsk', 'female');
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Irina', '32', 'Moscow', 'female');
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Pavel', '27', 'Volgograd', 'male');
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Kirill', '54', 'Tyumen', 'male');
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Sergey', '17', 'Moscow', 'male');
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Maxim', '22', 'Krasnoyarsk', 'female');
-INSERT INTO groupmates (name, age, address, floor) VALUES ('Nastya', '27', 'Moscow', 'female');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Clark', 25, 'Moscow', 'male');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Dave', 17, 'Yakutsk', 'male');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Ava', 26, 'Novosibirsk', 'female');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Sasha', 46, 'St. Petersburg', 'male');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Petya', 30, 'Neryungri', 'male');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Masha', 18, 'Omsk', 'female');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Irina', 32, 'Moscow', 'female');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Pavel', 27, 'Volgograd', 'male');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Kirill', 54, 'Tyumen', 'male');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Sergey', 17, 'Moscow', 'male');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Maxim', 22, 'Krasnoyarsk', 'female');
+INSERT INTO groupmates (name, age, address, floor) VALUES ('Nastya', 27, 'Moscow', 'female');
 
 -- fetch 
 -- SELECT * FROM groupmates WHERE age != '27';
